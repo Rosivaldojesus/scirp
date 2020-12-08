d626cb9730a9a9efa24e2d829b793497a37494d9
Rosivaldojesus <js.rosivaldo@gmail.com>
2020-12-08 13:07:03 -0300

    make it better

--- programa/urls.py
+++ programa/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth import views
 
 urlpatterns = [
+    path('login/', views.LoginView.as_view(template_name='usuarios/login.html'), name="login"),
+    path('logout/', views.LogoutView.as_view(), name="logout"),
+
     path('', Index),
     path('cftv-shopping/', Cftv_shopping),
     path('cftv-shopping-create/', Cftv_shopping_create),
--- programa/views.py
+++ programa/views.py
@@ -1,20 +1,29 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
-from .forms import CftvShoppingForm
+from .forms import *
 
+# Autenticação
+from django.contrib.auth.decorators import permission_required, login_required
+
+
+
+
+#-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
 # Create your views here.
+@login_required(login_url='/login/')
 def Index(request):
     return render(request, 'index.html')
 
 
-#-------------------------------------------------------------------------
-#-------------------------------------------------------------------------
-#-------------------------------------------------------------------------
+@login_required(login_url='/login/')
 def Cftv_shopping(request):
     camera = Cameras.objects.all()
     return render(request, 'cftv-shopping.html', {'camera': camera})
 
 
+@login_required(login_url='/login/')
 def Cftv_shopping_create(request):
     form = CftvShoppingForm(request.POST)
     if form.is_valid():
@@ -26,6 +35,7 @@ def Cftv_shopping_create(request):
     return render(request, 'cftv-shopping-create.html', {'form': form})
 
 
+@login_required(login_url='/login/')
 def Cftv_shopping_update(request, id=None):
     camera = get_object_or_404(Cameras, id=id)
     form = CftvShoppingForm(request.POST or None, instance=camera)
@@ -34,21 +44,24 @@ def Cftv_shopping_update(request, id=None):
         obj.save()
         return redirect('/cftv-shopping/')
     return render(request, 'cftv-shopping-update.html', {'form': form})
-   
-
 #-------------------------------------------------------------------------
 #-------------------------------------------------------------------------
 #-------------------------------------------------------------------------
 
 
+@login_required(login_url='/login/')
 def Automacao(request):
     gerenciadoras = Gerenciadoras.objects.all()
     controladoras = Sap.objects.all()
-    return render(request, 'automacao.html',{'gerenciadoras': gerenciadoras, 'controladoras':controladoras } )
+    return render(request, 'automacao.html',{'gerenciadoras': gerenciadoras, 'controladoras':controladoras })
 
+
+@login_required(login_url='/login/')
 def Sdai_shopping(request):
     return render(request, 'sdai-shopping.html')
 
+
+@login_required(login_url='/login/')
 def Sdai_torre_empresarial(request):
     return render(request, 'sdai-torre-empresarial.html')
 
